355598e78d9bb50e03e070d41699b9e0f0fb1901
savannajmaddin <savann@stud.ntnu.no>
2024-12-01 19:51:05 +0100

    omd

--- tests/testHandler.cpp
+++ tests/testHandler.cpp
@@ -2,13 +2,3 @@
 #include "Handler.hpp"
 
 #include <catch2/catch_test_macros.hpp>
-/*
-
-TEST_CASE( "er forholdet mellom visual.visualJoints og chain.visualJoints like ",   "[]") {
-
-    Scene scene;
-    KinematicChain chain;
-    VisualJoints visualJoints;
-
-}
-*/
